f9755913b5afd436a2f6fa9411d59cc30ff4d9c1
Elva Tristan <elva@mblz.com>
2015-01-05 12:54:36 -0600

    Automated commit at 2015-01-05 18:54:36 UTC by middleman-deploy 0.3.0

--- js/all.js
+++ js/all.js
@@ -252,16 +252,16 @@ jQuery.extend({
 });
 var $l = console.log.bind(console);
 
-function log(){
-	$.each(arguments, function(){
-		$('#logContainer').prepend(this + "<br/>")
-	})
-}
-$(function(){
-	$('#aLog').on('click', function(){
-		$('logContainer').toggle()
-	})
-})
+// function log(){
+// 	$.each(arguments, function(){
+// 		$('#logContainer').prepend(this + "<br/>")
+// 	})
+// }
+// $(function(){
+// 	$('#aLog').on('click', function(){
+// 		$('logContainer').toggle()
+// 	})
+// })
 ;
 $(function(){
   
---
+++ js/fb.js
@@ -0,0 +1,33 @@
+ $(function(){
+	 var imgRef = new Firebase("https://brilliant-fire-1525.firebaseio.com/images");
+	 var $images, $current = null
+	 imgRef.on("value", function(snapshot) {
+	 	$images  = snapshot.val().urls
+	 	$current = snapshot.val().current
+	 	setCurrent($current)
+	  //console.log($images, $current);
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
+
+	imgRef.on("child_changed", function(snapshot) {
+	  var changed = snapshot.val();
+	  setCurrent(changed)
+	  console.log("child changed", changed);
+	}); 
+
+	$('img.rt').on('click', function(){
+		var updated = null
+		if(this.src.indexOf('manta') == -1){
+			updated = 'manta'
+		} else { updated = 'shark'}
+		imgRef.update({current: updated})
+	})
+
+	function setCurrent(current){
+		$('#imgGit').attr('src','../img/rt/' + current + '.png')
+	}
+
+ })
+
+;
